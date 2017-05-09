ea8d2e29bff3fba82f2ccd2a6d3bd880f8e29232
Jiseung <Jiseung@192.168.219.103>
2017-05-10 02:53:13 +0900

    wqwqwqw

--- gonggan/src/main/webapp/js/index2.js
+++ gonggan/src/main/webapp/js/index2.js
@@ -1,3 +1,12 @@
 function like(obj,writerId, postId) {
-	
+	$.ajax({
+	      url: "/gonggan/postlist.do",
+	      data: {},
+	      success: function(data) {
+	    	  callbackList(data);
+	      },
+	      error: function(data,status,error){
+	         console.log("error : " + error);
+	      }
+	   });
 }
