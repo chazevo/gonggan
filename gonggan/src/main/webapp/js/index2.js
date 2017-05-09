7caba9ee3a8a4bafd16009d6af50eac217f34f73
jiseung <jiseung@218.38.137.28>
2017-05-10 02:54:27 +0900

    마지맊!!!!!!!!

--- gonggan/src/main/webapp/js/index2.js
+++ gonggan/src/main/webapp/js/index2.js
@@ -3,7 +3,6 @@ function like(obj,writerId, postId) {
 	      url: "/gonggan/postlist.do",
 	      data: {},
 	      success: function(data) {
-	    	  callbackList(data);
 	      },
 	      error: function(data,status,error){
 	         console.log("error : " + error);
